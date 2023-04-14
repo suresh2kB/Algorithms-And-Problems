3c410b4f887cebecaba9aa2ccc6a5dc03076c9c1
SURESH KUMAR <66684186+suresh2kB@users.noreply.github.com>
2023-04-14 15:42:26 +0530

    Create Algo_EularCycle.cpp

---
+++ Algo_EularCycle.cpp
@@ -0,0 +1,52 @@
+/**
+ *    author:  Suresh Kumar
+ **/
+#include <bits/stdc++.h>
+using namespace std;
+
+const int N = 1e5 + 1;
+vector<int> adj[N];
+
+void dfs(int u, int p)
+{
+    cout << u << " ";
+
+    for (int v : adj[u])
+    {
+        if (v != p)
+        {
+            dfs(v, u);
+        }
+    }
+    cout << u << " ";
+}
+void solve()
+{
+    int n;
+    cin >> n;
+    for (int i = 0; i < n - 1; i++)
+    {
+        int u, v;
+        cin >> u >> v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+
+    dfs(1, 0);
+}
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t = 1;
+    // cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+}
