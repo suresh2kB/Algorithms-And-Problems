b34becabc9ca0a60792510a4fd48b84456725b36
SURESH KUMAR <66684186+suresh2kB@users.noreply.github.com>
2024-04-13 19:26:06 +0530

    Update Prob_AllPathFromSourceToDest.cpp, removed visited array.

--- Prob_AllPathFromSourceToDest.cpp
+++ Prob_AllPathFromSourceToDest.cpp
@@ -1,30 +1,26 @@
 class Solution {
 public:
-    void dfs(int u,vector<int> vis,vector<int> curr,vector<vector<int>> &ans,vector<vector<int>> graph)
+    void dfs(int u,vector<int> curr,vector<vector<int>> &ans,vector<vector<int>> graph)
     {
         curr.push_back(u);
         if(u==graph.size()-1)
         {
             ans.push_back(curr);
+            return;
         }
-        vis[u] = 1;
         
         for(int v : graph[u])
         {
-            if(!vis[v])
-            {
-                dfs(v,vis,curr,ans,graph);
-            }
+            dfs(v,curr,ans,graph);
         }
     }
     vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
         int n = graph.size();
-        vector<int> vis(n);
         vector<vector<int>> ans;
 
         vector<int> curr;
-        dfs(0,vis,curr,ans,graph);
-
+        dfs(0,curr,ans,graph);
+        
         return ans;
     }
 };
