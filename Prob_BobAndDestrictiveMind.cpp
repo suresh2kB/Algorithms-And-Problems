6f3ed172fed3768afd32325d684ab37876bc8cad
SURESH KUMAR <66684186+suresh2kB@users.noreply.github.com>
2024-04-13 20:05:52 +0530

    Added question description

--- Prob_BobAndDestrictiveMind.cpp
+++ Prob_BobAndDestrictiveMind.cpp
@@ -1,6 +1,25 @@
 /**
  *    author:  Suresh Kumar
  **/
+
+Problem
+Rhezo has bought a graph with N nodes and M edges. He has a destructive mind and often deletes some nodes(and of course its edges to other nodes of the graph).
+
+He will ask you Q queries. Each query is denoted by an integer X, meaning that he will delete the node X. Since he has a destructive mind, he gets satisfied if the the new graph with deleted node has more number of connected components than the original graph.
+
+If satisfied, he wants you to print "Satisfied"(without quotes) and the number of edges that were removed when deleting node X, else print "Not Satisfied"(without quotes). All queries are different, and the original graph is restored after each query.
+
+Input:
+
+First line contains N and M as described in the problem statement. Each of the next M lines contain 2 integers 
+ and 
+, meaning that there is an edge between node A and node B. Next line contains a single integer Q, denoting the number of queries. Each of the next Q lines contain a single integer X as described in the problem statement.
+
+Output:
+
+For each of the Q queries, if Rhezo is satisfied(that is the number of connected components of the new graph has increased), print "Satisfied"(without quotes), and the number of edges that will be deleted after a space. Else print "Not Satisfied".
+
+
 #include <bits/stdc++.h>
 using namespace std;
 
